777f5d39b7cf4924823ca6aa13609a85292d617e
melissa-stott <1princessmelissa@gmail.com>
2021-02-19 21:59:52 -0800

    set app to use handlebars

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.use(express.static(path.join(__dirname, '/')));
+app.set('view engine', 'handlebars');
+
 
 
 
